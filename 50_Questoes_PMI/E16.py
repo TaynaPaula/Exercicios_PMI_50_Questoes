035d7a3d78709b8ac3a7a5e52f361d299fd091f9
TaynaPaula <taynapaulacruz@gmail.com>
2025-09-16 10:25:01 -0300

    Add

--- 50_Questoes_PMI/E16.py
+++
@@ -1,8 +0,0 @@
-#Crie um programa que converta segundos em horas, minutos e segundos.
-a=int(input("Digite a quantidade de segundos: "))
-hora = a // 3600                
-resto = a % 3600              
-minutos = resto // 60            
-segundos = resto % 60            
-
-print(f"{a} segundos equivalem a {hora}h:{minutos}m:{segundos}s")
