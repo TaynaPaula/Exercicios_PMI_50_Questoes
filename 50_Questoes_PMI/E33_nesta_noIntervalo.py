3a58b0b924b3d8006d56a916b4d22472baed42e0
TaynaPaula <taynapaulacruz@gmail.com>
2025-09-18 00:55:32 -0300

    Add

--- 50_Questoes_PMI/E33_nesta_noIntervalo.py
+++ 50_Questoes_PMI/E33_nesta_noIntervalo.py
@@ -1,20 +1,18 @@
-#Crie um programa que verifique se um número está em um intervalo
-n =0
-while n < 40 or n > 50:
+n = 9
+while True:
     try:
-        s = (input("digite um valor no intervalo de [40, 50]:"))
-        n = int (s)
-    except:
-        print("{} não é um número.".format(s))
-        n=0
-        
-    else:
-        if n <40 or n >50:
-            print("o valor lido {} esta forra do intervalo" .format(n))
+        palpite = int(input("Digite um número entre 0 e 100: "))
+        if palpite < 0 or palpite > 10:
+            print("Número fora do intervalo. Tente novamente.")
+            continue
+
+        if palpite == n:
+            print("Você acertou!")
+            
+            break
+        elif palpite < n:
+            print("O número é maior")
         else:
-            print("o valor lido {} esta ok" .format(n))
-            break 
-        
-    finally:
-        print("\n\n");#repetição do código
-    
+            print("O número é menor")
+    except ValueError:
+        print("Valor inválido! Por favor, digite um número válido.")
