37f5c0cb235e4e9fd316c20cf41c931394c80247
TaynaPaula <taynapaulacruz@gmail.com>
2025-09-18 01:00:05 -0300

    Add

--- 50_Questoes_PMI/E33_nesta_noIntervalo.py
+++ 50_Questoes_PMI/E33_nesta_noIntervalo.py
@@ -1,18 +1,21 @@
-n = 9
+n = 9  
+tentativas = 0
+
 while True:
     try:
         palpite = int(input("Digite um número entre 0 e 100: "))
-        if palpite < 0 or palpite > 10:
+        tentativas += 1
+
+        if palpite < 0 or palpite > 100:
             print("Número fora do intervalo. Tente novamente.")
             continue
 
         if palpite == n:
-            print("Você acertou!")
-            
+            print(f"Você acertou! O número era {n}. Tentativas: {tentativas}")
             break
         elif palpite < n:
-            print("O número é maior")
+            print("O número é maior.")
         else:
-            print("O número é menor")
+            print("O número é menor.")
     except ValueError:
         print("Valor inválido! Por favor, digite um número válido.")
